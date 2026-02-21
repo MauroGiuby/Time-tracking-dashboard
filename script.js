bec911e179dda9704eb7874b9bce3731163e8f2a
Mauro Giannubilo <167360342+MauroGiuby@users.noreply.github.com>
2026-02-21 10:02:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ let data = [
 const buttons = document.querySelectorAll('.activity-tracker-option')
 
 buttons.forEach(button => {
-  
+  button.addEventListener('click', () => { 
+    activateClickedButton(button)
+    const clickedOption = button.dataset.option
+  })
 })
