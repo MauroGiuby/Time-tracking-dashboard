4dc0d67d1f4a3043bf11346cfb5ebc1484e1b7a6
Mauro Giannubilo <167360342+MauroGiuby@users.noreply.github.com>
2026-03-03 18:11:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -45,34 +45,34 @@ const renderCards = (clickedOption) => {
     const section = document.createElement('section')
     section.classList.add('activity-tracker-activity', activityClass)
     const stringToInject = `
-        <div class="activity-bg">
-          <img src="./images/icon-${activityClass}.svg" alt="">
-        </div>
-        <div class="activity-info">
-          <header class="activity-header">
-            <h2 class="activity-name">
-              ${name}
-            </h2>
-            <button class="activity-options" aria-label="More options">
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="#BBC0FF" fill-rule="evenodd" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="5" cy="12" r="2"/>
-                <circle cx="12" cy="12" r="2"/>
-                <circle cx="19" cy="12" r="2"/>
-              </svg>
-            </button>
-          </header>
-          <div class="activity-timeframes">
-            <h3 class="activity-current-timeframes">
-              32hrs
-            </h3>
-            <div class="activity-previous-timeframes">
-              <p class="time-window">Last Week</p>
-              <p> - </p>
-              <p class="time">36hrs</p>
-            </div>
+      <div class="activity-bg">
+        <img src="./images/icon-${activityClass}.svg" alt="">
+      </div>
+      <div class="activity-info">
+        <header class="activity-header">
+          <h2 class="activity-name">
+            ${name}
+          </h2>
+          <button class="activity-options" aria-label="More options">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="#BBC0FF" fill-rule="evenodd" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="5" cy="12" r="2"/>
+              <circle cx="12" cy="12" r="2"/>
+              <circle cx="19" cy="12" r="2"/>
+            </svg>
+          </button>
+        </header>
+        <div class="activity-timeframes">
+          <h3 class="activity-current-timeframes">
+            ${timeframeData.current}hrs
+          </h3>
+          <div class="activity-previous-timeframes">
+            <p class="time-window">Last Week</p>
+            <p> - </p>
+            <p class="time">36hrs</p>
           </div>
-        </div>´
-    
+        </div>
+      </div>
+    `
   });
 };
 
